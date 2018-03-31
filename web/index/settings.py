7477dfc8a66fcd4dc4cae4dab964cadb346ff7f6
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-03-31 19:54:54 +0900

    add allowed_host setting
    avoid DISALLOWEDHOST error

--- web/index/settings.py
+++ web/index/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'me-*^^^itz_*@$i%9#4cnm$zf&a8&1n*)gn%@heb7tt71ldg!s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
