ae965d490fd0c79d106c183db929d77863a8d9c1
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-03-31 19:52:57 +0900

    add document save function
    update func is to save document file and text on database

--- web/docsearch/views.py
+++ web/docsearch/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+from docsearch.models import File 
+from docsearch.utils.update import find_all_files
+
 # Create your views here.
 def top(request):
     return render(request, 'docsearch/top.html', {'top': 'update here'})
 
 def update(request):
-    return render(request, 'docsearch/update.html', {'top': 'update here'})
+    tmp = 'bad'
+    for file in find_all_files('/home/jovyan/data/'):
+        tmp = 'partially ok'
+        if file[-3:] == '.md':
+            tmp = 'ok'
+            f = open(file)
+            File(filename=file, text=f.read()).save()
+            f.close()
+    return render(request, 'docsearch/update.html', {'finish': tmp})
