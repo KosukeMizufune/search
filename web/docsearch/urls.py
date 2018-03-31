c7626957fc19ae825d84e95d733a0d9ebbf644f0
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-03-31 18:19:35 +0900

    add update-url

--- web/docsearch/urls.py
+++ web/docsearch/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'docsearch'
 urlpatterns = [
     path('', views.top, name='top'),
+    path('update/', views.update, name='update'),
 ]
