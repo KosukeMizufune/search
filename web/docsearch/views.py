9387f48305f76750246049d037c35b20b4b8eb78
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-03-31 18:20:32 +0900

    add update-result view
    temporary view

--- web/docsearch/views.py
+++ web/docsearch/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def top(request):
     return render(request, 'docsearch/top.html', {'top': 'update here'})
+
+def update(request):
+    return render(request, 'docsearch/update.html', {'top': 'update here'})
