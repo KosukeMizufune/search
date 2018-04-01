b7e090c3118b2a03c1f2efa7d5559f7b0a92fab1
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-04-01 21:09:26 +0900

    change import order
    following docstring

--- web/docsearch/views.py
+++ web/docsearch/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
 from docsearch.models import File 
 from docsearch.utils.update import find_all_files
-from django.views import generic
 
 # Create your views here.
 def top(request):
