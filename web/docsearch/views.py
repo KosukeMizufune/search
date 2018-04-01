74504185ddb54956ea2deb302c66f863924e5ecd
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-04-01 20:52:41 +0900

    add view function
    add SearchView and link

--- web/docsearch/views.py
+++ web/docsearch/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 from docsearch.models import File 
 from docsearch.utils.update import find_all_files
+from django.views import generic
 
 # Create your views here.
 def top(request):
-    return render(request, 'docsearch/top.html', {'top': 'update here'})
+    return render(request, 'docsearch/top.html', {'top': 'update here', 'search': 'search here'})
 
 def update(request):
     tmp = 'bad'
@@ -17,3 +18,7 @@ def update(request):
             File(filename=file, text=f.read()).save()
             f.close()
     return render(request, 'docsearch/update.html', {'finish': tmp})
+
+class SearchView(generic.ListView):
+    model = File
+    template_name = 'docsearch/search.html'
