85afbd10d8d953e49b928080067e9a51c78eb6df
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-04-02 15:56:36 +0900

    add url
    response url

--- web/docsearch/urls.py
+++ web/docsearch/urls.py
@@ -6,5 +6,8 @@ app_name = 'docsearch'
 urlpatterns = [
     path('', views.top, name='top'),
     path('update/', views.update, name='update'),
-    path('search/', views.SearchView.as_view(), name='search'),
+    path('update_result/', views.update_result, name='update_result'),
+    path('search/', views.search, name='search'),
+    path('search_result/', views.SearchView.as_view(), name='search_result'),
+    
 ]
