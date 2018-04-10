76ad598d742bd81715dc1c3d1c99bc4c3c090a5c
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-04-10 14:30:02 +0900

    add url to text
    added page shows textfile which is searched

--- web/docsearch/urls.py
+++ web/docsearch/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
     path('update/', views.update, name='update'),
     path('update_result/', views.UploadView.as_view(), name='update_result'),
-    path('search_result/', views.SearchView.as_view(), name='search_result')]
+    path('search_result/', views.SearchView.as_view(), name='search_result'),
+    path('<int:pk>/search_text/', views.TextView.as_view(), name='search_text')
+]
