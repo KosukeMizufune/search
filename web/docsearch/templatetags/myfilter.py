08d66b4add115d4756e75e7393b1eace517cca4e
水船公輔 <kosuke@mizubunekousukenoMacBook-Pro.local>
2018-04-20 01:01:24 +0900

    add get_filename function
    this function gets filename not path

--- web/docsearch/templatetags/myfilter.py
+++ web/docsearch/templatetags/myfilter.py
@@ -1,3 +1,5 @@
+import re
+
 import markdown
 from django.template.defaultfilters import stringfilter
 from django import template
@@ -10,3 +12,7 @@ register = template.Library()
 def mark2html(value):
     return markdown.markdown(value, extensions=['fenced_code', 'codehilite(css_class=highlight)', 'mdx_math'])
 
+@register.filter
+@stringfilter
+def get_filename(value):
+    return re.findall(r'([^/]+?)?$', value)[0]
